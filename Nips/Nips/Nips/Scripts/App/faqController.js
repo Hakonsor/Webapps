12c5a904fdfaa6dc87e81695b4a1bb8b99218fe9
Hakonsor <hakonsor@gmail.com>
2015-11-11 14:28:48 +0100

    ferdig ish

--- Nips/Nips/Nips/Scripts/App/faqController.js
+++ Nips/Nips/Nips/Scripts/App/faqController.js
@@ -4,7 +4,7 @@ App.controller("faqController", function ($scope, $http) {
 
     var url = '/api/FAQ/';
     $scope.sletteFeil = false;
-
+    $scope.laster = true;
     function sporsmalListe() {
         $http.get(url).success(function (alleSporsmal) {
             $scope.alleSporsmal = alleSporsmal;
@@ -13,11 +13,52 @@ App.controller("faqController", function ($scope, $http) {
             console.log(status + data);
         });
     };
-    $scope.FAQ = true;
-    $scope.Sporsmal = true;
-    $scope.Liste = true;
-    $scope.laster = true;
-    sporsmalListe();
+
+    
+    $scope.emailPattern = "/^[a-zA-ZæÆøØåÅ][\w\.-]*[a-zA-Z0-9æÆøØåÅ]@[a-zA-ZæÆøØåÅ][\w\.-]*[a-zA-Z0-9æÆøØåÅ]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/";
+    
+    
+    start = function () {
+        $scope.email = "";
+        $scope.beskrivelse = "";
+        $scope.FAQ = true;
+        $scope.Sporsmal = false;
+        $scope.Liste = false;
+        $scope.kategori1 = true;
+        $scope.kategori2 = false;
+        $scope.kategori3 = false;
+        $scope.kategori4 = false;
+    };
+    start();
+
+    $scope.viskategori1 = function () {
+ 
+        $scope.kategori1 = true;
+        $scope.kategori2 = false;
+        $scope.kategori3 = false;
+        $scope.kategori4 = false;
+    };
+
+    $scope.viskategori2 = function () {
+        $scope.kategori2 = true;
+        $scope.kategori1 = false;
+        $scope.kategori3 = false;
+        $scope.kategori4 = false;
+    };
+
+    $scope.viskategori3 = function () {
+        $scope.kategori3 = true;
+        $scope.kategori2 = false;
+        $scope.kategori1 = false;
+        $scope.kategori4 = false;
+    };
+
+    $scope.viskategori4 = function () {
+        $scope.kategori4 = true;
+        $scope.kategori2 = false;
+        $scope.kategori3 = false;
+        $scope.kategori1 = false;
+    };
 
     $scope.visSporsmal = function () {
         $scope.email = "";
@@ -26,19 +67,23 @@ App.controller("faqController", function ($scope, $http) {
         $scope.Sporsmal = true;
         $scope.Liste = false;
     };
+
     $scope.visFAQ = function () {
         $scope.email = "";
         $scope.beskrivelse = "";
         $scope.FAQ = true;
         $scope.Sporsmal = false;
         $scope.Liste = false;
+        $scope.viskategori1();
     };
+
     $scope.visListe = function () {
         $scope.email = "";
         $scope.beskrivelse = "";
         $scope.Liste = true;
         $scope.FAQ = false;
         $scope.Sporsmal = false;
+        sporsmalListe();
     };
 
     $scope.setTempid = function (id) {
@@ -55,7 +100,7 @@ App.controller("faqController", function ($scope, $http) {
         $http.post(url, Spørsmal).
             success(function (data) {
                 sporsmalListe();
-                $scope.visFAQ;
+                $scope.laster = false;
                 //vis knapper
             }).
         error(function (data, status) {
@@ -75,8 +120,19 @@ App.controller("faqController", function ($scope, $http) {
             $tempId = 0;
         });
     };
+    $scope.sletteSporsmal = function () {
+        $http.delete(url + $scope.tempid).
+        success(function (data) {
+            sporsmalListe();
+            $tempId = 0;
+        }).
+        error(function (data, status) {
+            console.log(status + data);
+            $tempId = 0;
+        });
+    };
    
-
+  
 
 
 });
